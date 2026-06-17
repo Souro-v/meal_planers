ae0f97120143c5426e6edcd577b91dd0088bca50
sourov <sourov.kpi21@gmail.com>
2026-06-17 11:11:23 +0600

    onboarding

---
+++ lib/features/onboarding/controller/onboarding_controller.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../app/routes/app_routes.dart';
+
+class OnboardingController extends GetxController {
+  final pageController = PageController();
+  final currentPage    = 0.obs;
+
+  void onPageChanged(int index) => currentPage.value = index;
+
+  void nextPage() {
+    if (currentPage.value < 2) {
+      pageController.nextPage(
+        duration: const Duration(milliseconds: 350),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      Get.offAllNamed(AppRoutes.home);
+    }
+  }
+
+  void skip() => Get.offAllNamed(AppRoutes.home);
+
+  @override
+  void onClose() {
+    pageController.dispose();
+    super.onClose();
+  }
+}
